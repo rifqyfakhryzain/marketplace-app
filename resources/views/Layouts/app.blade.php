96bd65514895c859b653d1bafcd2da347d2ff8ee
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-01-24 10:25:02 +0700

    Ganti Href dari cdn adi vite

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -1,47 +1,55 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <title>LUNO</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
 </head>
 
 <body class="bg-gray-50 min-h-screen flex flex-col">
 
-@include('components.navbar')
+    @include('components.navbar')
 
-<main class="flex-1">
-    @yield('content')
-</main>
+    <main class="flex-1">
+        @yield('content')
+    </main>
 
-{{-- AUTH MODAL (GLOBAL) --}}
-@include('auth.modal')
+    {{-- AUTH MODAL (GLOBAL) --}}
+    @include('auth.modal')
 
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-<script>
-new Swiper(".adsSwiper", {
-    slidesPerView: 3,
-    spaceBetween: 16,
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: { slidesPerView: 1.2 },
-        640: { slidesPerView: 2 },
-        1024: { slidesPerView: 3 }
-    }
-});
-</script>
+    <script>
+        new Swiper(".adsSwiper", {
+            slidesPerView: 3,
+            spaceBetween: 16,
+            loop: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 1.2
+                },
+                640: {
+                    slidesPerView: 2
+                },
+                1024: {
+                    slidesPerView: 3
+                }
+            }
+        });
+    </script>
 
-@include('components.footer')
+    @include('components.footer')
 
 </body>
+
 </html>
