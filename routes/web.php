8305ef6f64a8d9a39017d3322143bfe2a2cc0d6a
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-01-24 16:51:36 +0700

    Revisi Routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\GoogleAuthController;
-use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,10 +39,8 @@ Route::middleware('auth')->group(function () {
         ->name('profile.update');
 
     Route::delete('/profile', [ProfileController::class, 'destroy'])
-        ->name('profile.destroy');  
-    });
-
-    
+        ->name('profile.destroy');
+});
 
 /*
 |--------------------------------------------------------------------------
