58cfabdb323b022e920f97990e281fcb38e781c8
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-01-24 16:44:36 +0700

    Menambahkan user penjual dan admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,13 +15,42 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // Pembeli
-        $buyer = User::firstOrCreate([
-            'email' => 'pembeli@gmail.com'],
+        $buyer = User::firstOrCreate(
+            [
+                'email' => 'pembeli@gmail.com'
+            ],
             [
                 'name' => 'Pembeli',
                 'password' => Hash::make('pembeli'),
                 'email_verified_at' => now(),
-            ]);
-            $buyer->assignRole('buyer');
+            ]
+        );
+        $buyer->assignRole('buyer');
+
+        // Penjual
+        $seller = User::firstOrCreate(
+            [
+                'email' => 'penjual@gmail.com'
+            ],
+            [
+                'name' => 'Penjual',
+                'password' => Hash::make('penjual'),
+                'email_verified_at' => now(),
+            ]
+        );
+        $seller->assignRole('seller');
+
+        // Penjual
+        $admin = User::firstOrCreate(
+            [
+                'email' => 'admin@gmail.com'
+            ],
+            [
+                'name' => 'admin',
+                'password' => Hash::make('admin'),
+                'email_verified_at' => now(),
+            ]
+        );
+        $admin->assignRole('admin');
     }
 }
