3e472770dfceb87d3a6df06dab6a2c3f4e51b42d
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-01-27 06:00:04 +0700

    Menambahkan tabel migrasi chats

---
+++ database/migrations/2026_01_26_220730_create_chats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('buyer_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('seller_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('barang_id')->constrained('barang')->cascadeOnDelete();
+            $table->timestamps();
+
+            $table->unique(['buyer_id','seller_id','barang_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
