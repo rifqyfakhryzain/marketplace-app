5b38c6617f8810a04042f3481a6f92cc0f8e96ca
Muhammad Shahril Ramdan <bloodwings40@gmail.com>
2026-01-26 14:36:00 +0700

    menambahkan controller barang

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Barang;
+
+class BarangController extends Controller
+{
+    public function index()
+    {
+        return Barang::all();
+    }
+
+    public function show($id)
+    {
+        return Barang::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $barang = \App\Models\Barang::find($id);
+
+        if (!$barang) {
+            return response()->json(['message' => 'Barang tidak ditemukan'], 404);
+        }
+
+        $barang->update($request->all());
+
+        return $barang;
+    }
+
+    public function destroy($id)
+    {
+        $barang = \App\Models\Barang::find($id);
+
+        if (!$barang) {
+            return response()->json(['message' => 'Barang tidak ditemukan'], 404);
+        }
+
+        $barang->delete();
+
+        return response()->json(['message' => 'Barang berhasil dihapus']);
+    }
+
+
+
+}
