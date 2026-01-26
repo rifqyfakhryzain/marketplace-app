efcd883fb8ef10c296b675e076ac061293f0f934
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-01-27 05:58:03 +0700

    Revisi pada controller order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -13,31 +13,32 @@ class OrderController extends Controller
     public function checkout(Request $request)
     {
         $request->validate([
-            'seller_id' => 'required|exists:,id',
-            'total_price' => 'required|numeric|min::1',
+            'seller_id' => 'required|exists:users,id',
+            'total_price' => 'required|numeric|min:1',
             'payment_method' => 'required|in:transfer_bank,cod',
         ]);
 
+        abort_if(Auth::id() == $request->seller_id, 403);
+
         DB::transaction(function () use ($request) {
-            // Buat Order
             $order = Order::create([
-                'buyer_id' => auth::id(),
+                'buyer_id' => Auth::id(),
                 'seller_id' => $request->seller_id,
                 'total_price' => $request->total_price,
                 'status' => $request->payment_method === 'cod'
-                ? 'on_process'
-                : 'pending_payment'
-                
+                    ? 'on_process'
+                    : 'pending_payment',
             ]);
 
-            // Buat payment
             Payment::create([
                 'order_id' => $order->id,
                 'method' => $request->payment_method,
                 'status' => $request->payment_method === 'cod'
-                ? 'unpaid'
-                : 'pending',
+                    ? 'unpaid'
+                    : 'pending',
             ]);
         });
+
+        return redirect()->back()->with('success', 'Order berhasil dibuat');
     }
 }
