5d36537c5f26bdad2774644c1484e92bb5f52314
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-01-27 18:47:29 +0700

    Menambahkan middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SetUserMode;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+         $middleware->append(SetUserMode::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
