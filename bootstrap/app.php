8d2438556829b3c2def7a7551099bc2fa1e8b660
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-01-29 14:36:53 +0700

    menambahkan middleware seller

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,20 +1,24 @@
 <?php
 
-use App\Http\Middleware\SetUserMode;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-         $middleware->append(SetUserMode::class);
-    })
+->withMiddleware(function (\Illuminate\Foundation\Configuration\Middleware $middleware): void {
+
+    $middleware->alias([
+        'seller' => \App\Http\Middleware\SellerOnly::class,
+    ]);
+
+})
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
