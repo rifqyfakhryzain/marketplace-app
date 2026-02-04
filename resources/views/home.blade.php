a02bc1ad33c3ce8e806a51ab62960577966a6074
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-05 06:23:30 +0700

    Revisi views home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
+    @include('components.ads-slider')
 
-@include('components.ads-slider')
-
-<div class="mb-16">
-    @include('components.categories')
-</div>
-
-<div class="max-w-7xl mx-auto px-6 mt-12">
-
-    <div class="flex items-center justify-between mb-3">
-        <h2 class="text-lg font-bold">Baru Diunggah</h2>
-        <a href="#" class="text-sm text-blue-600">Lihat semua</a>
+    <div class="mb-16">
+        @include('components.categories')
     </div>
 
-    <div class="overflow-x-auto">
-        <div class="flex gap-4 pb-2">
+    <div class="max-w-7xl mx-auto px-6 mt-12">
 
-            @foreach ($products as $product)
-                @include('components.product-card', ['product' => $product])
-            @endforeach
+        <div class="flex items-center justify-between mb-3">
+            <h2 class="text-lg font-bold">Baru Diunggah</h2>
+            <a href="#" class="text-sm text-blue-600">Lihat semua</a>
         </div>
+
+        <div class="overflow-x-auto">
+            <div class="flex gap-4 pb-2">
+
+                @foreach ($products as $product)
+                    @include('components.product-card', ['product' => $product])
+                @endforeach
+            </div>
+        </div>
+
     </div>
 
-</div>
-
-@include('components.rekomendasi')
-
-
+    @include('components.rekomendasi')
 @endsection
