21eddc7671eaca5b7263b42bcca48d4a73f5674f
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-05 06:23:01 +0700

    Revisi product card

--- resources/views/Components/product-card.blade.php
+++ resources/views/Components/product-card.blade.php
@@ -1,28 +1,27 @@
 @isset($product)
-<a href="{{ route('products.show', $product['id']) }}"
-   class="block w-[200px] shrink-0">
+    <a href="{{ route('products.show', $product->id) }}" class="block w-[200px] shrink-0">
 
-    <div class="bg-white rounded-xl shadow p-3
+        <div class="bg-white rounded-xl shadow p-3
                 hover:shadow-lg transition">
 
-        <img
-            src="{{ asset($product['image']) }}"
-            class="w-full h-[140px] object-cover rounded-lg"
-            alt="{{ $product['name'] }}"
-        >
+            <img src="{{ $product->images->first()
+                ? asset('storage/' . $product->images->first()->image_path)
+                : asset('images/placeholder-product.jpg') }}"
+                class="w-full h-[140px] object-cover rounded-lg" alt="{{ $product->nama_barang }}">
 
-        <p class="text-sm mt-2">
-            {{ $product['name'] }}
-        </p>
 
-        <p class="font-bold mt-1">
-            Rp {{ number_format($product['price'], 0, ',', '.') }}
-        </p>
+            <p class="text-sm mt-2 line-clamp-2">
+                {{ $product->nama_barang }}
+            </p>
 
-        <span class="text-xs text-gray-500">
-            {{ $product['location'] }}
-        </span>
+            <p class="font-bold mt-1">
+                Rp {{ number_format($product->harga, 0, ',', '.') }}
+            </p>
 
-    </div>
-</a>
+            <span class="text-xs text-gray-500">
+                {{ $product->penjual->alamat ?? 'Indonesia' }}
+            </span>
+
+        </div>
+    </a>
 @endisset
