c057fd8997455f8333896b9ab6bb16a7b694bedf
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-05 06:21:51 +0700

    Revisi public barang controller

--- app/Http/Controllers/PublicBarangController.php
+++ app/Http/Controllers/PublicBarangController.php
@@ -7,33 +7,38 @@ use Illuminate\Http\Request;
 
 class PublicBarangController extends Controller
 {
-public function show(Barang $barang)
-{
-    // Proteksi barang nonaktif
-    if ($barang->status !== 'tersedia') {
-        abort(404);
+    public function show(Barang $barang)
+    {
+        if ($barang->status !== 'tersedia') {
+            abort(404);
+        }
+
+        $barang->load(['images', 'penjual']);
+
+        $images = $barang->images->count()
+            ? $barang->images
+            ->map(fn($img) => asset('storage/' . $img->image_path))
+            ->toArray()
+            : [asset('images/placeholder-product.jpg')];
+
+        $product = [
+            'id' => $barang->id,
+            'name' => $barang->nama_barang,
+            'description' => $barang->deskripsi,
+            'price' => $barang->harga,
+            'location' => 'Indonesia',
+            'images' => $images,
+            'user' => [
+                'id' => $barang->penjual->id,
+                'name' => $barang->penjual->name,
+                'username' => $barang->penjual->username,
+                'avatar' => asset('images/avatar-placeholder.png'),
+            ],
+        ];
+
+        return view('public.barang.show', [
+            'product' => $product,
+            'ratings' => [],
+        ]);
     }
-
-    $product = [
-        'id' => $barang->id,
-        'name' => $barang->nama_barang,
-        'description' => $barang->deskripsi,
-        'price' => $barang->harga,
-        'location' => $barang->penjual->profile->alamat ?? 'Indonesia',
-        'images' => [
-            asset('images/placeholder-product.jpg'),
-        ],
-        'user' => [
-            'id' => $barang->penjual->id,
-            'name' => $barang->penjual->name,
-            'avatar' => asset('images/avatar-placeholder.png'),
-        ],
-    ];
-
-    return view('public.barang.show', [
-        'product' => $product,   
-        'ratings' => [],
-    ]);
-}
-
 }
