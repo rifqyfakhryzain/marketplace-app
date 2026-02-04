6c295b3e6a476402b147f1b0a26155d3c46f9630
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-04 10:36:17 +0700

    Revisi seeder users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,52 +5,42 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // Pembeli
-        $buyer = User::firstOrCreate(
-            [
-                'email' => 'pembeli@gmail.com'
-            ],
+        // BUYER
+        User::firstOrCreate(
+            ['email' => 'pembeli@gmail.com'],
             [
                 'name' => 'Pembeli',
                 'password' => Hash::make('pembeli'),
                 'email_verified_at' => now(),
+                'role' => 'buyer',
             ]
         );
-        $buyer->assignRole('buyer');
 
-        // Penjual
-        $seller = User::firstOrCreate(
-            [
-                'email' => 'penjual@gmail.com'
-            ],
+        // SELLER
+        User::firstOrCreate(
+            ['email' => 'penjual@gmail.com'],
             [
                 'name' => 'Penjual',
                 'password' => Hash::make('penjual'),
                 'email_verified_at' => now(),
+                'role' => 'seller',
             ]
         );
-        $seller->assignRole('seller');
 
-        // Penjual
-        $admin = User::firstOrCreate(
+        // ADMIN
+        User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
             [
-                'email' => 'admin@gmail.com'
-            ],
-            [
-                'name' => 'admin',
+                'name' => 'Admin',
                 'password' => Hash::make('admin'),
                 'email_verified_at' => now(),
+                'role' => 'admin',
             ]
         );
-        $admin->assignRole('admin');
     }
 }
