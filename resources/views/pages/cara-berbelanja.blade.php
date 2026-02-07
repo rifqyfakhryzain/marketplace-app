48b838e5dca567182df32f0fdf06c2aa14743f15
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-07 18:47:05 +0700

    Revisi view cara belanja

--- resources/views/pages/cara-berbelanja.blade.php
+++ resources/views/pages/cara-berbelanja.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="max-w-4xl mx-auto px-6 mt-12">
-    <h1 class="text-2xl font-bold mb-4">Cara Berbelanja</h1>
-    <p class="text-gray-600">
-        Halaman ini akan diisi konten cara berbelanja di kemudian hari.
-    </p>
-</div>
+    <div class="max-w-4xl mx-auto px-6 mt-12">
+        <h1 class="text-2xl font-bold mb-4">Cara Berbelanja</h1>
+        <p class="text-gray-600">
+            Halaman ini akan diisi konten cara berbelanja di kemudian hari.
+        </p>
+    </div>
 @endsection
