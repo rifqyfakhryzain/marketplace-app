cbd73a78502dc2beb4f903bb9a53046da96dcbb4
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-07 18:49:07 +0700

    Revisi view components product detail

--- resources/views/Components/product-detail.blade.php
+++ resources/views/Components/product-detail.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="max-w-4xl mx-auto px-6 mt-12">
+    <div class="max-w-4xl mx-auto px-6 mt-12">
 
-    <a href="/" class="text-sm text-blue-600">
-        ← Kembali
-    </a>
+        <a href="/" class="text-sm text-blue-600">
+            ← Kembali
+        </a>
 
-    <h1 class="text-2xl font-bold mt-4">
-        Detail Produk (ID: {{ $id }})
-    </h1>
+        <h1 class="text-2xl font-bold mt-4">
+            Detail Produk (ID: {{ $id }})
+        </h1>
 
-    <p class="mt-2 text-gray-600">
-        Halaman detail produk (dummy dulu).
-    </p>
+        <p class="mt-2 text-gray-600">
+            Halaman detail produk (dummy dulu).
+        </p>
 
-</div>
+    </div>
 @endsection
