cbec8f9d4e089bd7ca57d07029e95991994650f7
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-07 11:07:54 +0700

    Menambahkan Controller BUyer Payment

--- app/Http/Controllers/Buyer/BuyerPaymentController.php
+++ app/Http/Controllers/Buyer/BuyerPaymentController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Buyer;
 
-use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Escrow;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class BuyerPaymentController extends Controller
 {
+    
     public function show(Order $order)
     {
         // pastikan relasi barang ikut kebawa
@@ -21,4 +23,31 @@ class BuyerPaymentController extends Controller
             'escrow' => $escrow,
         ]);
     }
+
+public function confirmTransfer(Order $order)
+{
+    DB::transaction(function () use ($order) {
+
+        // ambil escrow (WAJIB ADA)
+        $escrow = $order->escrow;
+
+        if (!$escrow) {
+            abort(404, 'Escrow tidak ditemukan');
+        }
+
+        // update escrow
+        $escrow->update([
+            'status' => 'waiting_verification',
+        ]);
+
+        // update order
+        $order->update([
+            'status' => 'waiting_verification',
+        ]);
+    });
+
+    return redirect()
+        ->route('buyer.orders.pay', $order->id)
+        ->with('success', 'Konfirmasi transfer berhasil. Menunggu verifikasi admin.');
+}
 }
