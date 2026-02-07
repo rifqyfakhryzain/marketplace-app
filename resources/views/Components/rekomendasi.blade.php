2a66aea884a942ee10bed5167cf21db31df07333
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-07 18:49:32 +0700

    Revisi view components rekomendasi

--- resources/views/Components/rekomendasi.blade.php
+++ resources/views/Components/rekomendasi.blade.php
@@ -1,42 +1,42 @@
 @php
-$rekomendasi = [
-    [
-        'name' => 'Laptop Lenovo ThinkPad',
-        'price' => 7500000,
-        'location' => 'Jakarta Selatan',
-        'image' => 'images/produk.jpg',
-    ],
-    [
-        'name' => 'iPhone 11 128GB',
-        'price' => 6800000,
-        'location' => 'Bandung',
-        'image' => 'images/produk.jpg',
-    ],
-    [
-        'name' => 'Kamera Canon EOS M50',
-        'price' => 5200000,
-        'location' => 'Surabaya',
-        'image' => 'images/produk.jpg',
-    ],
-    [
-        'name' => 'Sepeda Gunung Polygon',
-        'price' => 3100000,
-        'location' => 'Malang',
-        'image' => 'images/produk.jpg',
-    ],
-    [
-        'name' => 'PlayStation 4 Slim',
-        'price' => 4200000,
-        'location' => 'Depok',
-        'image' => 'images/produk.jpg',
-    ],
-    [
-        'name' => 'Jam Tangan Seiko',
-        'price' => 1900000,
-        'location' => 'Yogyakarta',
-        'image' => 'images/produk.jpg',
-    ],
-];
+    $rekomendasi = [
+        [
+            'name' => 'Laptop Lenovo ThinkPad',
+            'price' => 7500000,
+            'location' => 'Jakarta Selatan',
+            'image' => 'images/produk.jpg',
+        ],
+        [
+            'name' => 'iPhone 11 128GB',
+            'price' => 6800000,
+            'location' => 'Bandung',
+            'image' => 'images/produk.jpg',
+        ],
+        [
+            'name' => 'Kamera Canon EOS M50',
+            'price' => 5200000,
+            'location' => 'Surabaya',
+            'image' => 'images/produk.jpg',
+        ],
+        [
+            'name' => 'Sepeda Gunung Polygon',
+            'price' => 3100000,
+            'location' => 'Malang',
+            'image' => 'images/produk.jpg',
+        ],
+        [
+            'name' => 'PlayStation 4 Slim',
+            'price' => 4200000,
+            'location' => 'Depok',
+            'image' => 'images/produk.jpg',
+        ],
+        [
+            'name' => 'Jam Tangan Seiko',
+            'price' => 1900000,
+            'location' => 'Yogyakarta',
+            'image' => 'images/produk.jpg',
+        ],
+    ];
 @endphp
 
 <div class="max-w-7xl mx-auto px-6 mt-12">
@@ -53,14 +53,12 @@ $rekomendasi = [
     <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 
         @foreach ($rekomendasi as $product)
-            <div class="bg-white rounded-xl shadow p-3
+            <div
+                class="bg-white rounded-xl shadow p-3
                         hover:shadow-lg transition cursor-pointer">
 
-                <img
-                    src="{{ asset($product['image']) }}"
-                    class="w-full h-[150px] object-cover rounded-lg"
-                    alt="{{ $product['name'] }}"
-                >
+                <img src="{{ asset($product['image']) }}" class="w-full h-[150px] object-cover rounded-lg"
+                    alt="{{ $product['name'] }}">
 
                 <p class="text-sm mt-2 line-clamp-2">
                     {{ $product['name'] }}
