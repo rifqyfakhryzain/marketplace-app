fe10b8e70c80e446b75ba3b5628da4e058597675
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-08 20:06:54 +0700

    Revisi views Pessanan

--- resources/views/pesanan.blade.php
+++ resources/views/pesanan.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="max-w-7xl mx-auto px-6 mt-12">
+    <div class="max-w-7xl mx-auto px-6 mt-12">
 
-    <h1 class="text-2xl font-bold mb-4">Pesanan Saya</h1>
+        <h1 class="text-2xl font-bold mb-4">Pesanan Saya</h1>
 
-    <div class="bg-white rounded-xl shadow p-6">
-        <p class="text-gray-600">
-            Belum ada pesanan.
-        </p>
+        <div class="bg-white rounded-xl shadow p-6">
+            <p class="text-gray-600">
+                Belum ada pesanan.
+            </p>
+        </div>
+
     </div>
-
-</div>
 @endsection
