697a39d873e2f76f8051c948a7ec2836e171ecbb
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-09 18:05:59 +0700

    revisi views profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="max-w-4xl mx-auto px-6 mt-12">
+    <div class="max-w-4xl mx-auto px-6 mt-12">
 
-    <h1 class="text-2xl font-bold mb-4">Profil Saya</h1>
+        <h1 class="text-2xl font-bold mb-4">Profil Saya</h1>
 
-    <div class="bg-white rounded-xl shadow p-6">
-        <p class="text-gray-600">Nama: Hennessy</p>
-        <p class="text-gray-600 mt-2">Email: dummy@email.com</p>
+        <div class="bg-white rounded-xl shadow p-6">
+            <p class="text-gray-600">Nama: Hennessy</p>
+            <p class="text-gray-600 mt-2">Email: dummy@email.com</p>
+        </div>
+
     </div>
-
-</div>
 @endsection
