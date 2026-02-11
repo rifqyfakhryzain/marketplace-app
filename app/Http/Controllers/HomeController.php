a8c73c2035edb4f0870968dbf42da91c696e8798
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-11 20:31:36 +0700

    Revisi controller home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Barang;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,13 +11,41 @@ class HomeController extends Controller
     // index
 public function index()
 {
+    $categories = Kategori::all();
+
     $products = Barang::public()
-        ->with(['penjual', 'images'])
+        ->with(['penjual', 'images', 'kategori'])
         ->latest()
         ->take(10)
         ->get();
 
-    return view('home', compact('products'));
+    $rekomendasi = Barang::public()
+        ->with(['penjual', 'images'])
+        ->inRandomOrder()
+        ->take(8)
+        ->get();
+
+    return view('home', compact('products', 'rekomendasi', 'categories'));
+}
+
+public function filterKategori($id)
+{
+    $categories = Kategori::all();
+
+    $products = Barang::public()
+        ->where('kategori_id', $id)
+        ->with(['penjual', 'images', 'kategori'])
+        ->latest()
+        ->get();
+
+    $rekomendasi = Barang::public()
+        ->where('kategori_id', $id) // 🔥 TAMBAH INI
+        ->with(['penjual', 'images'])
+        ->inRandomOrder()
+        ->take(8)
+        ->get();
+
+    return view('home', compact('products', 'rekomendasi', 'categories'));
 }
 
 
